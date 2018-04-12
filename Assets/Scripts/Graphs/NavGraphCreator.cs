96b07405d321ee9416bb5c0579fef269bcee3c46
MSI\user <andrej.senchov@murka.com>
2018-04-12 23:18:54 +0300

    [+] add point renderer and some sceene changes

--- Assets/Scripts/Graphs/NavGraphCreator.cs
+++ Assets/Scripts/Graphs/NavGraphCreator.cs
@@ -1,19 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Graphs;
 using UnityEngine;
 
-public class NavGraphCreator : MonoBehaviour
+namespace Graphs
 {
-    [SerializeField] private Transform StartPoint;
-    [SerializeField] private float MaxDistanceToNode;
-
-    private Graph<NavGraphNode, GraphEdge> Graph;
-
-    [ContextMenu("CreateGraph")]
-    public void CreateGraph()
+    public class NavGraphCreator : MonoBehaviour
     {
-        Graph = new Graph<NavGraphNode, GraphEdge>();
-        
+        [SerializeField] private Transform StartPoint;
+        [SerializeField] private float MaxDistanceToNode;
+        [SerializeField] private GameObject NavGraphPoint;
+
+        private Graph<NavGraphNode, GraphEdge> Graph;
+
+        private Vector3[] Directions = new[] {Vector3.right, Vector3.left, Vector3.down, Vector3.up};
+        private List<GameObject> Points;
+
+        [ContextMenu("CreateGraph")]
+        public void CreateGraph()
+        {
+            Graph = new Graph<NavGraphNode, GraphEdge>();
+
+            for (int i = 0; i < Directions.Length; i++)
+            {
+                if (!IsFindObstacle(Directions[i]))
+                {
+                    Vector3 pos = transform.position + MaxDistanceToNode * Directions[i];
+                    GameObject point = Instantiate(NavGraphPoint, pos, Quaternion.identity);
+                    Points.Add(point);
+                    point.SetActive(true);
+                }
+            }
+        }
+
+        [ContextMenu("ClearPoints")]
+        public void ClearPoints()
+        {
+            foreach (GameObject point in Points)
+            {
+                Destroy(point);
+            }
+
+            Points.Clear();
+        }
+
+        private bool IsFindObstacle(Vector3 dir)
+        {
+            return Physics.Raycast(transform.position, dir, MaxDistanceToNode);
+        }
     }
 }
---
+++ Assets/Scripts/Graphs/gizmos/PointRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Graphs
+{
+    public class PointRenderer : MonoBehaviour
+    {
+        [SerializeField] private float Radius;
+        public Vector3[] Points;
+        
+        private void OnDrawGizmos()
+        {
+          /*  for (int i = 0; i < Points.Length; i++)
+            {
+                Gizmos.DrawSphere(Points[i],Radius);
+            }*/
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(transform.position,Radius);
+        }
+    }
+}
