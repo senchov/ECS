68c72545959d38aa59d5dc68e24467c80c7b77be
andrej.senchov <andrej.senchov@murka.com>
2018-07-04 23:43:30 +0300

    [+] start work with steering behavior

---
+++ Assets/Core/SteeringBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringBehavior
+{
+    public float MaxSpeed;
+    public float MaxVelocity;
+
+    public SteeringBehavior(float maxSpeed, float maxVelocity)
+    {
+        MaxSpeed = maxSpeed;
+        MaxVelocity = maxVelocity;
+    }
+
+    public Vector3 Seek(Vector2 sourcePos, Vector2 target)
+    {
+        Vector2 desiredVelocity = (target - sourcePos).normalized * MaxSpeed;
+        return desiredVelocity;
+    }
+
+    public Vector3 Arrive(Vector2 sourcePos, Vector2 target, Vector2 sourceVel, float slowingRaiusSqr = 1)
+    {
+        Vector2 desiredVelocity = target - sourcePos;
+        float distance = desiredVelocity.sqrMagnitude;
+
+        if (distance < slowingRaiusSqr)
+            desiredVelocity = desiredVelocity.normalized  * (distance / slowingRaiusSqr) * MaxSpeed;
+        else
+        {
+            desiredVelocity = desiredVelocity.normalized * MaxSpeed ;
+        }
+        
+        return  desiredVelocity - sourceVel;
+    }
+}
---
+++ Assets/Scripts/MoveComponent.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveComponent : MonoBehaviour
+{
+    [SerializeField] private float MaxSpeed;
+    [SerializeField] private float MaxVel = 0.2f;
+
+    private SteeringBehavior SteeringBehavior;
+    private Vector3 Velocity = Vector3.zero;
+    private Vector3 Target = Vector3.zero;
+    private Vector3 Steer = Vector3.zero;
+
+    private void Start()
+    {
+        SteeringBehavior = new SteeringBehavior(MaxSpeed, MaxVel);
+        Target = transform.position;
+    }
+
+    private void Update()
+    {
+        SteeringBehavior.MaxSpeed = MaxSpeed;
+        SteeringBehavior.MaxVelocity = MaxVel;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.LogError("mousePos" + mousePos);
+            mousePos.z = 0;
+            Target = mousePos;
+        }
+
+        Vector2 sourcePos = new Vector2(transform.position.x, transform.position.y);
+        Steer = SteeringBehavior.Arrive(sourcePos, Target, Velocity);
+        Debug.LogError("st->" + Steer.ToString());
+
+        Velocity = Steer * Time.deltaTime;
+
+        Truncate(ref Velocity, MaxVel);
+        this.transform.position += Velocity;
+    }
+
+    private void Truncate(ref Vector3 velocity, float maxMagnitude)
+    {
+        float value = Mathf.Clamp01(maxMagnitude / velocity.magnitude);
+        velocity = velocity * value;
+    }
+}
