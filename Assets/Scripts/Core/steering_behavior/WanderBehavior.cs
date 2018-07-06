8125f5af19e9ad87d20eedc06c1998c741d8bf8d
andrej.senchov <andrej.senchov@murka.com>
2018-07-06 23:36:26 +0300

    [*+] add second wander and relocate scripts

---
+++ Assets/Scripts/Core/steering_behavior/WanderBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderBehavior
+{
+    public float Radius;
+    public float Distance;
+    public float Jitter;
+
+    Vector2 WanderTarget;
+    float WanderAngle;
+
+    public WanderBehavior(float radius, float distance, float jitter)
+    {
+        Radius = radius;
+        Distance = distance;
+        Jitter = jitter;
+    }
+
+    public Vector2 Wander(ref Vector3 velocity)
+    {
+        Vector2 circleCentr = velocity;
+        circleCentr = circleCentr.normalized;
+        circleCentr *= Distance;
+
+        Vector2 displacement = new Vector2(0,-1);
+        displacement *= Radius;
+
+        SetAngle(ref displacement,WanderAngle);
+
+        WanderAngle += Random.Range(-1.0f, 1.0f) * Jitter;
+
+        return circleCentr + displacement;
+    }
+
+    private void SetAngle(ref Vector2 displacement, float wanderAngle)
+    {
+        float lenght = displacement.magnitude;
+        displacement.x = Mathf.Cos(wanderAngle) * lenght;
+        displacement.y = Mathf.Sin(wanderAngle) * lenght;
+    }
+   
+}
---
+++ Assets/Scripts/Steering/WanderSecondComponent.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderSecondComponent : MonoBehaviour
+{
+    [SerializeField] private float WanderRadius = 2.0f;
+    [SerializeField] private float WanderDistance = 2.0f;
+    [SerializeField] private float Jitter = 10.0f;
+    [SerializeField] private float MaxVel = 10.0f;
+    [SerializeField] private float MinVelocity = 0.01f;
+    [SerializeField] private float WanderSpeed = 10.0f;
+
+    private WanderBehavior WanderBehavior;
+    private Vector3 Steer = Vector3.zero;
+    private Vector3 Velocity = Vector3.zero;
+    private float RotateSmooth;
+    private float MaxSmoothValue = 1.0f;
+
+    private void Start()
+    {
+       WanderBehavior = new WanderBehavior(WanderRadius,WanderDistance, Jitter);
+    }
+
+    private void Update()
+    {
+        Steer = WanderBehavior.Wander(ref Velocity) * WanderSpeed;
+
+        Velocity = Steer * Time.deltaTime;
+        Velocity = Vector3.ClampMagnitude(Velocity, MaxVel);
+        if (Velocity.sqrMagnitude < MinVelocity)
+            Velocity = Vector3.zero;
+
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        transform.position += Velocity;
+    }
+
+    private void Rotate()
+    {
+        float angle = Mathf.Atan2(-Velocity.x, Velocity.y) * Mathf.Rad2Deg;       
+        Quaternion target = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, angle);
+        transform.rotation = Quaternion.Lerp(transform.rotation,target, RotateSmooth);
+        RotateSmooth += Time.deltaTime;
+        if (RotateSmooth > MaxSmoothValue)
+            RotateSmooth = 0;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + Velocity*WanderDistance);
+    }
+}
