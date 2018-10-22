eb5fae21d004fc9d2a486deb7c31ba995be8756b
andrej.senchov <andrej.senchov@murka.com>
2018-10-22 23:45:33 +0300

    [*] continue work

--- Assets/Scripts/Fuzzy/RuleSet/ShootDecisionRuleSet.cs
+++ Assets/Scripts/Fuzzy/RuleSet/ShootDecisionRuleSet.cs
@@ -1,46 +1,48 @@
 ﻿using System.Collections.Generic;
 
-namespace Assets.Scripts.Fuzzy.RuleSet
+namespace Fuzzy
 {
     class ShootDecisionRuleSet
     {
-        private List<Rule> Rules;
-
         private int DistanceToTarget;
         private int Ammo;
+        private DecisionConsequent[,] DesireSet;
+        private float[,] ConclusionSet;
+        private FuzzyLinguisticVariables DistanceToTargetFLV;
+        private FuzzyLinguisticVariables AmmoFLV;
 
         public ShootDecisionRuleSet()
         {
-            Rules = new List<Rule>();
-            Rules.Add(new Rule (DistanceToTargetAntecedent.Far,AmmoAntecedent.Loads,DecisionConsequent.Desirable));
-            Rules.Add(new Rule(DistanceToTargetAntecedent.Far, AmmoAntecedent.Ok, DecisionConsequent.UnDesirable));
-            Rules.Add(new Rule(DistanceToTargetAntecedent.Far, AmmoAntecedent.Low, DecisionConsequent.UnDesirable));
-            Rules.Add(new Rule(DistanceToTargetAntecedent.Medium, AmmoAntecedent.Loads, DecisionConsequent.VeryDesirable));
-            Rules.Add(new Rule(DistanceToTargetAntecedent.Medium, AmmoAntecedent.Ok, DecisionConsequent.VeryDesirable));
-            Rules.Add(new Rule(DistanceToTargetAntecedent.Medium, AmmoAntecedent.Low, DecisionConsequent.Desirable));
-            Rules.Add(new Rule(DistanceToTargetAntecedent.Close, AmmoAntecedent.Loads, DecisionConsequent.UnDesirable));
-            Rules.Add(new Rule(DistanceToTargetAntecedent.Close, AmmoAntecedent.Ok, DecisionConsequent.UnDesirable));
-            Rules.Add(new Rule(DistanceToTargetAntecedent.Close, AmmoAntecedent.Low, DecisionConsequent.UnDesirable));
+            ConclusionSet = new float[3,3];
+
+            DesireSet = new DecisionConsequent[3, 3];
+            DesireSet[(int)DistanceToTargetAntecedent.Far, (int)AmmoAntecedent.Loads] = DecisionConsequent.Desirable;
+            DesireSet[(int)DistanceToTargetAntecedent.Far, (int)AmmoAntecedent.Ok] = DecisionConsequent.UnDesirable;
+            DesireSet[(int)DistanceToTargetAntecedent.Far, (int)AmmoAntecedent.Low] = DecisionConsequent.UnDesirable;
+            DesireSet[(int)DistanceToTargetAntecedent.Medium, (int)AmmoAntecedent.Loads] = DecisionConsequent.VeryDesirable;
+            DesireSet[(int)DistanceToTargetAntecedent.Medium, (int)AmmoAntecedent.Ok] = DecisionConsequent.VeryDesirable;
+            DesireSet[(int)DistanceToTargetAntecedent.Medium, (int)AmmoAntecedent.Low] = DecisionConsequent.Desirable;
+            DesireSet[(int)DistanceToTargetAntecedent.Close, (int)AmmoAntecedent.Loads] = DecisionConsequent.UnDesirable;
+            DesireSet[(int)DistanceToTargetAntecedent.Close, (int)AmmoAntecedent.Ok] = DecisionConsequent.UnDesirable;
+            DesireSet[(int)DistanceToTargetAntecedent.Close, (int)AmmoAntecedent.Low] = DecisionConsequent.UnDesirable;
+
+            IFuzzySet[] distanceFlVFuzzySets = new IFuzzySet[3];
+            distanceFlVFuzzySets[0] = new LeftShoulderFuzzySet(25, 150);
+            distanceFlVFuzzySets[1] = new TriangleFuzzySet(25, 150, 300);
+            distanceFlVFuzzySets[2] = new RightShoulderFuzzySet(150, 300);
+            DistanceToTargetFLV = new FuzzyLinguisticVariables(distanceFlVFuzzySets);
+
+            IFuzzySet[] ammoFlVFuzzySets = new IFuzzySet[3];
+            distanceFlVFuzzySets[0] = new LeftShoulderFuzzySet(0, 10);
+            distanceFlVFuzzySets[1] = new TriangleFuzzySet(0, 10, 30);
+            distanceFlVFuzzySets[2] = new RightShoulderFuzzySet(10, 30);
+            AmmoFLV = new FuzzyLinguisticVariables(ammoFlVFuzzySets);
         }
 
-        public void SetConditions(int distanceToTarget, int ammo)
+        private void Deffuzify(int distanceToTarget, int ammo)
         {
             DistanceToTarget = distanceToTarget;
             Ammo = ammo;
-        }
-
-        private class Rule
-        {
-            public Rule(DistanceToTargetAntecedent dis, AmmoAntecedent ammo , DecisionConsequent decision)
-            {
-                Distance = dis;
-                Ammo = ammo;
-                Decision = decision;
-            }
-
-            public DistanceToTargetAntecedent Distance;
-            public AmmoAntecedent Ammo;
-            public DecisionConsequent Decision;
         }
     }
 
---
+++ Assets/Scripts/Fuzzy/flv/FuzzyLinguisticVariables.cs
@@ -0,0 +1,17 @@
+﻿namespace Fuzzy
+{
+    class FuzzyLinguisticVariables
+    {
+        private IFuzzySet[] Sets;
+
+        public FuzzyLinguisticVariables(params IFuzzySet[] sets)
+        {
+            Sets = sets;
+        }
+
+        public IFuzzySet GetSet(int index)
+        {
+            return Sets[index];
+        }
+    }    
+}
